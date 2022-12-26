f094da633fb4e9271e208a2f8995f2b3d10e4fca
blackSooooo <blacksooooo@naver.com>
2022-12-26 16:55:04 +0900

    chore: cloud-config 의존성 추가 및 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,14 @@ plugins {
 group = "blacksooooo"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+val springCloudVersion: String by project
 
 repositories {
 	mavenCentral()
+}
+
+ext {
+	set("springCloudVersion", "2021.0.2")
 }
 
 dependencies {
@@ -20,7 +25,15 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+	}
 }
 
 tasks.withType<KotlinCompile> {
